312a184071be91a550d9464d2147e451f5c4a506
Mariano <alberto.martinez@kairosds.com>
2018-12-11 00:55:06 -0600

    master - feat(http interceptor, JWT): Interceptor added, jwt added in headers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { JwtProvider } from './core/providers/jwt.provider';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -14,6 +13,8 @@ import { ApiService } from './core/http/api.service';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginPage } from '../pages/login/login';
 import { SignupPage } from '../pages/signup/signup';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpTokenInterceptor } from './core/interceptors/http.token.interceptor';
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { SignupPage } from '../pages/signup/signup';
     SignupPage
   ],
   providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptor, multi: true },
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
--- src/app/core/providers/jwt.provider.ts
+++ src/app/core/providers/jwt.provider.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import * as JWT from 'jwt-decode';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtProvider {
 
+  tokenJWT: String = '';
+
   constructor(private storage: Storage) { }
 
-  getToken(): any {
-    this.storage.get('jwtToken').then((val) => {
-      return val;
-    });
+  getToken(): Promise<any> {
+    return this.storage.get('jwtToken');
   }
 
   saveToken(token: String) {
+    this.tokenJWT = token;
     this.storage.set('jwtToken', token);
   }
 
   saveUserId(token: any): Promise<any>{
+    this.saveToken(token);
     let userData = JWT(token);
     return this.storage.set('userId', userData['id']);
   }
